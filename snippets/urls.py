fe23c92ce0010ed8508f3b10570e44421858805d
shaddygarg <shaddygarg1@gmail.com>
2017-08-15 17:57:34 +0530

    Final testing

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import SnippetViewSet,UserViewSet
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 """
 snippet_list=SnippetViewSet.as_view({
@@ -23,6 +24,8 @@ user_list=UserViewSet.as_view(
         )
 user_detail=UserViewSet.as_view({'get':'retrieve'})
 """
+schema_view=get_schema_view(title='PastebinAPI')
+
 router=DefaultRouter()
 router.include_format_suffixes = False
 router.register(r'users',UserViewSet)
@@ -31,4 +34,5 @@ router.register(r'snippets',SnippetViewSet)
 urlpatterns =format_suffix_patterns([
         url(r'^',include(router.urls)),
         url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework')),
+        url(r'^schema/$',schema_view),
     ])
